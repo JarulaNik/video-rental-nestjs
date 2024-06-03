df3b0f1d90b9dc9b70a8ead3656978c5c1100fc0
Jaroslav Zhidkov <jaropolk1104@mail.ru>
2024-06-03 15:39:28 +0300

    .

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -5,10 +5,10 @@ const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
-  const [error, setError] = useState(null); // Добавляем состояние для ошибки
+  const [error, setError] = useState(null);
 
   const login = async (email, password) => {
-    setError(null); // Сбрасываем ошибку перед попыткой входа
+    setError(null);
     try {
       const response = await fetch('/api/auth/login', {
         method: 'POST',
@@ -19,11 +19,11 @@ export const AuthProvider = ({ children }) => {
       if (response.ok) {
         const data = await response.json();
         localStorage.setItem('token', data.accessToken);
-        setUser(data.user); // Сохраняем данные пользователя
+        setUser(data.user);
         setIsAuthenticated(true);
       } else {
-        const errorData = await response.json(); // Получаем данные об ошибке
-        setError(errorData.message || 'Ошибка авторизации'); // Устанавливаем ошибку
+        const errorData = await response.json();
+        setError(errorData.message || 'Ошибка авторизации');
       }
     } catch (error) {
       setError('Произошла ошибка при подключении к серверу.');
@@ -39,7 +39,7 @@ export const AuthProvider = ({ children }) => {
   const value = {
     isAuthenticated,
     user,
-    error, // Передаем ошибку в контекст
+    error,
     login,
     logout,
   };
