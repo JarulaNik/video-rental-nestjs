02b1b823e044d33ec78f32713754f78862a26ee4
Jaroslav Zhidkov <jaropolk1104@mail.ru>
2024-06-03 15:41:52 +0300

    .

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,19 +1,20 @@
-// MovieCard.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
 function MovieCard({ movie }) {
+  const imageUrl = `/images/${movie.id}.jpg`;
   return (
     <Link to={`/movies/${movie.id}`} className="movie-card-link">
       <div className="movie-card">
         <div className="movie-card-image-container">
-          <img src={movie.imageUrl} alt={movie.title} className="movie-card-image" />
+          <img src={imageUrl} alt={movie.title} className="movie-card-image" />
         </div>
         <div className="movie-card-content">
           <h3 className="movie-card-title">{movie.title}</h3>
-          <p className="movie-card-genre">{movie.genre || "Жанр не указан"}</p> {/* Выводим "Жанр не указан", если movie.genre пустой */}
-          {/* ... другие поля, которые вы хотите отображать ... */}
+          <p className="movie-card-genre">{movie.genre || "Жанр не указан"}</p>
+
         </div>
       </div>
     </Link>
