72ca51699769acd9fa20799c683a6ffd0d08223a
Jaroslav Zhidkov <jaropolk1104@mail.ru>
2024-06-03 15:43:42 +0300

    .

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import './Navigation.css'; // Добавьте стили при необходимости
+import './Navigation.css';
 
 const Navigation = () => {
   const { isAuthenticated } = useAuth();
